eefc0984ac669fcd5f631f06caa5a9b3f1f5e6b7
Frans-Lukas <franslukas@gmail.com>
2019-10-03 09:06:06 +0200

    Added date range selectors

--- topskills/forms.py
+++ topskills/forms.py
@@ -1,5 +1,11 @@
+import datetime
+
 from django import forms
 from bootstrap_datepicker_plus import DatePickerInput
 
 class DateInput(forms.DateInput):
     input_type = 'date'
+
+class DateForm(forms.Form):
+    start = forms.DateField(widget=DateInput, initial=datetime.date.today)
+    end = forms.DateField(widget=DateInput, initial=datetime.date.today)
--- topskills/views.py
+++ topskills/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from django.shortcuts import render
 
 # Create your views here.
+from topskills.forms import DateForm
 from topskills.models import Keyword
 
 
@@ -9,7 +10,18 @@ def index(request):
     # group by keyword and count occurrence
     # SELECT keyword, COUNT(keyword) AS total FROM topskills_keyword GROUP BY keyword;
     keywordsCount = Keyword.objects.values("keyword").annotate(total=Count('keyword'))
+    date_form = DateForm
     context = {
-        'keywordsCount': keywordsCount
+        'keywordsCount': keywordsCount,
+        'date_form': date_form,
     }
     return render(request, 'topskills/index.html', context)
+
+def change_date(request):
+    form = DateForm(request.GET)
+    if form.start.is_valid():
+        start_date = form.start.cleaned_data['date']
+
+    if form.end.is_valid():
+        end_date = form.end.cleaned_data['date']
+    print(end_date)
