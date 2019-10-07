450e323a79d32abf4af3bd43701b574ffa90256a
Frans-Lukas <franslukas@gmail.com>
2019-10-07 20:07:42 +0200

    Fixed date forms!

--- topskills/views.py
+++ topskills/views.py
@@ -1,28 +1,41 @@
+from datetime import timedelta, datetime
+
 from django.db.models import Count
 from django.db.models.functions import Coalesce
 from django.shortcuts import render
 
 # Create your views here.
 from topskills.forms import DateForm
-from topskills.models import Keyword
+from topskills.models import Keyword, Job
 
 
 def index(request):
-    # group by keyword and count occurrence
-    # SELECT keyword, COUNT(keyword) AS total FROM topskills_keyword GROUP BY keyword;
-    keywordsCount = Keyword.objects.values("keyword").annotate(total=Count('keyword')).order_by('-total')
-    date_form = DateForm
+    start_date = datetime.today()
+    end_date = datetime.today()
+
+    form = DateForm(request.GET)
+    if form.is_valid():
+        start_date = form.cleaned_data['start']
+        end_date = form.cleaned_data['end']
+
+
+    form.fields['start'].initial = start_date
+    form.fields['end'].initial = end_date
+
+    day_before_start = start_date - timedelta(days=1)
+    day_after_end = end_date + timedelta(days=1)
+    keywordsCount = Job.objects.raw('SELECT keyword, keyword AS id, count(keyword)'
+                                    ' FROM topskills_job'
+                                    ' INNER JOIN topskills_keyword'
+                                    ' ON topskills_job .id = topskills_keyword.job_hash_id'
+                                    ' WHERE date_found'
+                                    ' BETWEEN \'' + day_before_start.strftime("%Y-%m-%d") + '\''
+                                    ' AND \'' + day_after_end.strftime("%Y-%m-%d") + '\''
+                                    'GROUP BY keyword;')
     context = {
         'keywordsCount': keywordsCount,
-        'date_form': date_form,
+        'date_form': form,
+        'start_date': start_date,
+        'end_date': end_date,
     }
     return render(request, 'topskills/index.html', context)
-
-def change_date(request):
-    form = DateForm(request.GET)
-    if form.start.is_valid():
-        start_date = form.start.cleaned_data['date']
-
-    if form.end.is_valid():
-        end_date = form.end.cleaned_data['date']
-    print(end_date)
