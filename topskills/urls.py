10c015e52b14363b7ba701bc00dde7c3080be186
Frans-Lukas <franslukas@gmail.com>
2019-10-23 15:51:52 +0200

    changed so url is encoded correctly and main page is correct

--- topskills/urls.py
+++ topskills/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'topskills'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('jobs-with-word/<slug:word>', views.jobs, name='jobs')
+    path('jobs-with-word/<str:word>', views.jobs, name='jobs')
 ]
