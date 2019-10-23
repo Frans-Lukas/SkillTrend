ccd695c4e9e980c9e970738c1baa58fe2cd64fb1
Frans-Lukas <franslukas@gmail.com>
2019-10-23 14:41:35 +0200

    added new field in table.

--- topskills/models.py
+++ topskills/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Job(models.Model):
     job_hash = models.CharField(max_length=255, unique=True)
     job_description = models.CharField(max_length=1000, default='')
+    job_url = models.CharField(max_length=2000, default='')
     date_found = models.DateTimeField()
     date_viable_to = models.DateTimeField(default=datetime.today())
 
